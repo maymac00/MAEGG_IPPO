fe7f80b7ac8f3c5ea88bf43036e6a28c96c97677
Mayo <arnau.mayoral10@gmail.com>
2023-10-17 10:16:17 +0200

    add

---
+++ job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=16:00:00
+#SBATCH --job-name="Gathering"
+# export PATH=/mnt/beegfs/iiia/arnau_mayoral/conda/envs/Gathering/bin:~/conda/bin:$PATH
+
+export PATH=~/beegfs/anaconda3/envs/MAEGG_IPPO/bin:~/beegfs/anaconda3/bin:$PATH
+source activate MAEGG_IPPO
+export PYTHONUNBUFFERED=TRUE
+
+# srun python ippo.py --tag big_map_first_tries --seed 1 --tot-steps 5000000 --early-stop 2500000 --actor-lr 0.003 --critic-lr 0.01 --map-size big --n-agents 5 --survival-threshold 50 --donation-capacity 25 --apple-regen 0.002
+# srun python run_policy_PPO.py
+
+./upgrade_libs.sh > /dev/null
+
+echo "Running job $SLURM_JOB_ID"
+echo "Code used for IPPO:"
+curl -s "https://api.github.com/repos/maymac00/Independent_PPO/commits" | grep -E -m 1 '"sha"|"date"' | sed -E 's/.*: "(.*)".*/\1/'
+echo "Code used for MAEGathering:"
+curl -s "https://api.github.com/repos/maymac00/MultiAgentEthicalGatheringGame/commits" | grep -E -m 1 '"sha"|"date"' | sed -E 's/.*: "(.*)".*/\1/'
+echo "===================="
+srun python main.py
--- upgrade_libs.sh
+++ upgrade_libs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source /home/arnau/tools/python_venv/MAEGG_IPPO/bin/activate
-pip uninstall multiagentethicalgathering
+###source /home/arnau/tools/python_venv/MAEGG_IPPO/bin/activate
+pip uninstall -y multiagentethicalgathering
 pip install git+https://github.com/maymac00/MultiAgentEthicalGatheringGame.git
-pip uninstall ippo
+pip uninstall -y ippo
 pip install git+https://github.com/maymac00/Independent_PPO.git
