6648eebb99b2bbc3017cdc484f2accdc92453599
Mayo <arnau.mayoral10@gmail.com>
2023-10-25 09:51:14 +0200

    test.py

--- test.py
+++ test.py
@@ -5,19 +5,17 @@ import gym
 
 env = gym.make("MultiAgentEthicalGathering-v1", **tiny)
 
-agents = IPPO.agents_from_file("EGG_DATA/tiny/2500_30000_1")
+agents = IPPO.agents_from_file("data/tiny/2500_30000_1")
 
-env.track = True
-env.stash_runs = True
+env.setTrack(True)
+env.setStash(True)
 env.reset()
-print("1234")
-for r in range(10):
+for r in range(100):
     obs, _ = env.reset()
     acc_reward = [0] * env.n_agents
     for i in range(env.max_steps):
         actions = [agent.predict(obs[i]) for i, agent in enumerate(agents)]
 
         obs, reward, done, info = env.step(actions)
-        print(info)
-        # env.render()
+        env.render()
 env.plot_results("line")
