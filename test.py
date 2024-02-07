9e78e25527507cd850591f795111f269db690ae7
arnau <arnau.mayoral10@gmail.com>
2024-02-07 17:08:38 +0100

    test prints mo

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import numpy as np
 from EthicalGatheringGame import MAEGG, NormalizeReward
 from EthicalGatheringGame.presets import tiny, small, medium, large
 from IndependentPPO import IPPO
@@ -12,7 +13,7 @@ large["we"] = [1, 0.91]
 env = gym.make("MultiAgentEthicalGathering-v1", **large)
 # env = NormalizeReward(env)
 
-agents = IPPO.actors_from_file("jro/EGG_DATA/ethical_large_we0_try3/ethical_large_we0_try3/2500_80000_1_(8)")
+agents = IPPO.actors_from_file("EGG_DATA/ethical_large_we10_try2/ethical_large_we10_try2/2500_80000_1_(29)")
 env.setTrack(True)
 env.setStash(True)
 env.reset()
@@ -27,8 +28,7 @@ for r in range(1000):
         obs, reward, done, info = env.step(actions)
         acc_reward = [acc_reward[i] + reward[i] for i in range(env.n_agents)]
         # env.render(mode="partial_observability")
-
-    print(f"Epsiode {r}: {acc_reward} \t Agents (V_0, V_e): ", "\t".join([f"({env.agents[i].r_vec})" for i in range(env.n_agents)]))
+    print(f"Epsiode {r}: {acc_reward} \t Agents (V_0, V_e): ", "\t\t".join([f"{np.round(env.agents[i].r_vec)}" for i in range(env.n_agents)]))
     mo_history.append([env.agents[i].r_vec for i in range(env.n_agents)])
     history.append(acc_reward)
 
