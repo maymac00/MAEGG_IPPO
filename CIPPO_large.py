9c94ea51b90097e1bffd38e7aa39ceb68dc01ddb
maymac00 <arnau.mayoral10@gmail.com>
2024-04-02 09:17:52 +0200

    easter push

--- CIPPO_large.py
+++ CIPPO_large.py
@@ -1,19 +1,20 @@
 import copy
 
 import gym
+import numpy as np
 from EthicalGatheringGame import NormalizeReward
 from EthicalGatheringGame.presets import large
 
 from IndependentPPO.callbacks import TensorBoardLogging, SaveCheckpoint, Report2Optuna, AnnealEntropy, \
     PrintAverageReward
-from IndependentPPO import ParallelCIPPO
+from IndependentPPO import ParallelCIPPO, CIPPO
 from IndependentPPO.lr_schedules import DefaultPPOAnnealing, IndependentPPOAnnealing
 
 from hyper_tuning import OptimizerMAEGG, OptimizerMOMAGG
 from IndependentPPO.config import args_from_json
 
 
-class LargeSizeOptimize(OptimizerMAEGG):
+class LargeSizeCOptimize(OptimizerMAEGG):
     def __init__(self, direction, env_config, ppo_config, study_name=None, save=None, n_trials=1, pruner=None):
         super().__init__(direction, env_config, ppo_config, study_name, save, n_trials,
                          pruner)
@@ -21,11 +22,11 @@ class LargeSizeOptimize(OptimizerMAEGG):
     def pre_objective_callback(self, trial):
         self.args = copy.deepcopy(self.ppo_config)
         self.args.save_dir += "/" + self.study_name
-        self.args.const_limit_1 = 3
-        self.args.const_limit_2 = 3
+        self.args.const_limit_1 = trial.suggest_int("const_limit_1", 2, 6)
+        self.args.const_limit_2 = trial.suggest_int("const_limit_2", 2, 6)
         # self.args.ent_coef = trial.suggest_float("ent_coef", 0.001, 0.1, step=0.001)
-        self.args.ent_coef = 0.02
-        self.args.tot_steps = 30000000
+        self.args.ent_coef = 0.04
+        self.args.tot_steps = 40000000
 
     def construct_ppo(self, trial):
         env = gym.make("MultiAgentEthicalGathering-v1", **self.env_config)
@@ -36,21 +37,30 @@ class LargeSizeOptimize(OptimizerMAEGG):
             trial.set_user_attr(k, v)
 
         ppo = ParallelCIPPO(self.args, env=env)
+        # We make groups of efficiency to reduce the amount of parameters to tune.
+        eff_groups = [np.where(env.efficiency == value)[0].tolist() for value in np.unique(env.efficiency)]
+        eff_dict = {}
+        for k in eff_groups[0]:
+            eff_dict[k] = {"actor_lr": 6e-06, "critic_lr": 0.0008}
+        for k in eff_groups[1]:
+            eff_dict[k] = {"actor_lr": 6e-06, "critic_lr": 0.0007}
+        """
+        for i, group in enumerate(eff_groups):
+            actorlr = trial.suggest_float(f"actor_lr_{i}", 0.000001, 0.00001, step=0.000005)
+            criticlr = trial.suggest_float(f"critic_lr_{i}", 0.0002, 0.001, step=0.0001)
+            for agent in group:
+                eff_dict[agent] = {"actor_lr": actorlr, "critic_lr": criticlr}
+        """
+        ppo.lr_scheduler = IndependentPPOAnnealing(ppo, eff_dict)
 
-        ppo.lr_scheduler = IndependentPPOAnnealing(ppo, {
-            k: {
-                "actor_lr": trial.suggest_float(f"actor_lr_{k}", 0.00001, 0.0001, step=0.00001),
-                "critic_lr": trial.suggest_float(f"critic_lr_{k}", 0.0002, 0.001, step=0.0001)
-            } for k in range(self.args.n_agents)
-        })
-
-        # self.args.mult_lr = trial.suggest_float("lagran_multi_lr", 0.005, 0.07, step=0.015)
-        # self.args.mult_init = trial.suggest_float("lagran_multi_init", 0.3, 0.8, step=0.1)
+        self.args.mult_lr = trial.suggest_float("lagran_multi_lr", 0.005, 0.07, step=0.015)
+        self.args.mult_init = trial.suggest_float("lagran_multi_init", 0.3, 0.8, step=0.1)
         self.args.concavity_entropy = 2.0
-        final_value = trial.suggest_float("final_value", 0.0, 1.0, step=0.2)
+        # final_value = trial.suggest_float("final_value", 0.2, 1.0, step=0.2)
+        final_value = 0.4
         ppo.addCallbacks([
-            PrintAverageReward(ppo, n=5000),
-            TensorBoardLogging(ppo, log_dir=f"{args.save_dir}/{args.tag}/log/{ppo.run_name}", f=50),
+            PrintAverageReward(ppo, n=20, show_time=True),
+            TensorBoardLogging(ppo, log_dir=f"{args.save_dir}/{args.tag}/log/{ppo.run_name}", f=25),
             SaveCheckpoint(ppo, 5000),
             AnnealEntropy(ppo, final_value=final_value, concavity=self.args.concavity_entropy),
         ])
@@ -68,7 +78,14 @@ class LargeSizeOptimize(OptimizerMAEGG):
 if __name__ == "__main__":
     args = args_from_json("hyperparameters/large.json")
     # parse we from the args.tag string. Example: "mediumwe0.9_try1" -> we = 0.9
-    large["efficiency"] = [0.2] * 3 + [0.85] * 2
-    large["donation_capacity"] = 10
-    opt = LargeSizeOptimize("maximize", large, args, n_trials=1, save=args.save_dir, study_name=args.tag)
+    eff_rate = 0.8
+    large["efficiency"] = [0.85]*int(args.n_agents*eff_rate) + [0.2]*int(args.n_agents - eff_rate * args.n_agents)
+    print(large["efficiency"])
+    args.tag += f"_effrate{eff_rate}"
+    try:
+        db = float(args.tag.split("db")[1].split("_")[0])
+    except:
+        db = 5
+    large["donation_capacity"] = db
+    opt = LargeSizeCOptimize("maximize", large, args, n_trials=1, save=args.save_dir, study_name=args.tag)
     opt.optimize()
