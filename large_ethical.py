eb4bb10c153ec04508161c379b7d3520dc81d0fd
maymac00 <arnau.mayoral10@gmail.com>
2024-06-03 16:16:51 +0200

    args seed overwrite bug

--- large_ethical.py
+++ large_ethical.py
@@ -1,5 +1,5 @@
 import copy
-
+import time
 import gym
 import numpy as np
 from EthicalGatheringGame import NormalizeReward
@@ -17,6 +17,8 @@ class LargeSizeOptimize(OptimizerMAEGG):
     def __init__(self, direction, env_config, ppo_config, study_name=None, save=None, n_trials=1, pruner=None):
         super().__init__(direction, env_config, ppo_config, study_name, save, n_trials,
                          pruner)
+        # Sleep for random time to avoid choosing the same arguments on case of concurrent runs.
+        time.sleep(np.random.randint(0, 10))
 
     def pre_objective_callback(self, trial):
         self.args = copy.deepcopy(self.ppo_config)
@@ -30,28 +32,31 @@ class LargeSizeOptimize(OptimizerMAEGG):
         for k, v in self.env_config.items():
             trial.set_user_attr(k, v)
 
-        ppo = ParallelIPPO(self.args, env=env)
-        self.args.ent_coef = trial.suggest_float("ent_coef", 0.06, 0.08, step=0.02)
-        self.args.tot_steps = 50000000
+        self.args.ent_coef = 0.08
 
         # We make groups of efficiency to reduce the amount of parameters to tune.
         eff_groups = [np.where(env.efficiency == value)[0].tolist() for value in np.unique(env.efficiency)]
         eff_dict = {}
-        """
+
         for k in eff_groups[0]:
-            eff_dict[k] = {"actor_lr": 6e-06, "critic_lr": 0.0008}
+            eff_dict[k] = {"actor_lr": 1e-06, "critic_lr": 700e-06}
         for k in eff_groups[1]:
-            eff_dict[k] = {"actor_lr": 6e-06, "critic_lr": 0.0007}
+            eff_dict[k] = {"actor_lr": 1e-06, "critic_lr": 700e-06}
         """
         for i, group in enumerate(eff_groups):
             actorlr = trial.suggest_float(f"actor_lr_{i}", 0.000001, 0.00001, step=0.000005)
             criticlr = trial.suggest_float(f"critic_lr_{i}", 0.0002, 0.001, step=0.0001)
             for agent in group:
                 eff_dict[agent] = {"actor_lr": actorlr, "critic_lr": criticlr}
+        """
+        self.args.seed = trial.suggest_int("seed", 2, 20)
 
-        ppo.lr_scheduler = IndependentPPOAnnealing(ppo, eff_dict)
         self.args.concavity_entropy = 2.0
         final_value = 0.4
+
+        # Do not modify args after this point.
+        ppo = ParallelIPPO(self.args, env=env)
+        ppo.lr_scheduler = IndependentPPOAnnealing(ppo, eff_dict)
         ppo.addCallbacks([
             PrintAverageReward(ppo, n=5000, show_time=True),
             TensorBoardLogging(ppo, log_dir=f"{args.save_dir}/{args.tag}/log/{ppo.run_name}", f=50),
